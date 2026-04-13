03c821b4e42f656f50030807c625e2f89d20856d
Harshit <harshit.kumar@pealorganisation.com>
2026-04-13 12:58:39 +0530

    ok

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 const generateAccessToken = (user) => {
+    // Agar env se nahi mila to temporary string use hogi (sirf test ke liye)
+    const secret = process.env.ACCESS_TOKEN_SECRET || "TEMP_SECRET_KEY_FOR_TESTING";
+    
     return jwt.sign(
         {
             _id: user._id,
             email: user.email,
             role: user.role
         },
-        process.env.ACCESS_TOKEN_SECRET,
+        secret,
         {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY || '7d'
         }
     );
 };
-
-module.exports = { generateAccessToken };
