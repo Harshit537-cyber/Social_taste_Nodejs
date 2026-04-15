d86469bb1915db278ce2ac14df2652f5528e159d
Harshit <harshit.kumar@pealorganisation.com>
2026-04-15 12:53:26 +0530

    ok

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const userRouter = require('./src/routes/user.routes');
 
 const app = express();
 
-// Middlewares
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+
+app.use(cors()); 
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
